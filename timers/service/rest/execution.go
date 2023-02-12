80236ce234ba714feb05d477b01b3326c1b05f8a
Antonio Alexander <antonio-alexander@users.noreply.github.com>
2023-02-12 16:58:39 -0600

    Trying to fix flakey tests/functionality with changes

--- timers/service/rest/execution.go
+++ timers/service/rest/execution.go
@@ -1,51 +1,8 @@
 package rest
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/antonio-alexander/go-bludgeon/timers/data"
-	"github.com/antonio-alexander/go-bludgeon/timers/meta"
-
-	internal_errors "github.com/antonio-alexander/go-bludgeon/internal/errors"
-
-	"github.com/pkg/errors"
 )
-
-func handleResponse(writer http.ResponseWriter, err error, bytes []byte) error {
-	if err != nil {
-		var e internal_errors.Error
-
-		switch {
-		default:
-			writer.WriteHeader(http.StatusInternalServerError)
-		case errors.Is(err, meta.ErrTimerNotFound):
-			writer.WriteHeader(http.StatusNotFound)
-		case errors.Is(err, meta.ErrTimerNotUpdated):
-			writer.WriteHeader(http.StatusNotModified)
-		case errors.Is(err, meta.ErrTimerConflictCreate) || errors.Is(err, meta.ErrTimerConflictUpdate):
-			writer.WriteHeader(http.StatusConflict)
-		}
-		switch i := err.(type) {
-		case internal_errors.Error:
-			e = i
-		default:
-			e = internal_errors.New(err.Error())
-		}
-		bytes, err = json.Marshal(&e)
-		if err != nil {
-			return err
-		}
-		_, err = writer.Write(bytes)
-		return err
-	}
-	if bytes == nil {
-		writer.WriteHeader(http.StatusNoContent)
-	}
-	writer.Header().Set("Content-Type", "application/json; charset=utf-8")
-	_, err = writer.Write(bytes)
-	return err
-}
 
 func idFromPath(pathVariables map[string]string) string {
 	id, ok := pathVariables[data.PathID]
