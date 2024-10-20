8fa0966edac34f1b7437b111b61b9a4a3ab3f005
Antonio Alexander <antonio-alexander@users.noreply.github.com>
2024-10-20 02:06:17 -0500

    Added logger

---
+++ pkg/logger/go.mod
@@ -0,0 +1,8 @@
+module github.com/antonio-alexander/go-bludgeon/pkg/logger
+
+go 1.21.5
+
+require (
+	github.com/antonio-alexander/go-bludgeon/common v1.0.0
+	github.com/antonio-alexander/go-bludgeon/pkg/config v1.0.0
+)
---
+++ pkg/logger/logger.go
@@ -0,0 +1,88 @@
+package logger
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	common "github.com/antonio-alexander/go-bludgeon/common"
+	config "github.com/antonio-alexander/go-bludgeon/pkg/config"
+)
+
+type logger struct {
+	*log.Logger
+	config *Configuration
+}
+
+func New() interface {
+	Logger
+	Printer
+	common.Configurer
+} {
+	config := new(Configuration)
+	config.Default()
+	return &logger{config: new(Configuration)}
+}
+
+func (l *logger) Configure(items ...interface{}) error {
+	var c *Configuration
+
+	for _, item := range items {
+		switch v := item.(type) {
+		case config.Envs:
+			c = new(Configuration)
+			c.FromEnv(v)
+		case *Configuration:
+			c = v
+		}
+	}
+	if c == nil {
+		return errors.New(config.ErrConfigurationNotFound)
+	}
+	l.config = c
+	l.Logger = log.New(os.Stdout, fmt.Sprintf("[%s] ", l.config.Prefix), log.Ltime|log.Ldate|log.Lmsgprefix)
+	l.Info("logger configured with level \"%s\"", l.config.Level)
+	return nil
+}
+
+func (l *logger) Error(format string, v ...interface{}) {
+	if l.config.Level >= Error {
+		l.Printf("[error] "+format, v...)
+	}
+}
+
+func (l *logger) Info(format string, v ...interface{}) {
+	if l.config.Level >= Info {
+		l.Printf("[info] "+format, v...)
+	}
+}
+
+func (l *logger) Debug(format string, v ...interface{}) {
+	if l.config.Level >= Debug {
+		l.Printf("[debug] "+format, v...)
+	}
+}
+
+func (l *logger) Trace(format string, v ...interface{}) {
+	if l.config.Level >= Trace {
+		l.Printf("[trace] "+format, v...)
+	}
+}
+
+type nullLogger struct{}
+
+func NewNullLogger(parameters ...interface{}) interface {
+	Logger
+	Printer
+} {
+	return &nullLogger{}
+}
+
+func (n *nullLogger) Error(format string, v ...interface{})  {}
+func (n *nullLogger) Info(format string, v ...interface{})   {}
+func (n *nullLogger) Debug(format string, v ...interface{})  {}
+func (n *nullLogger) Trace(format string, v ...interface{})  {}
+func (n *nullLogger) Print(v ...interface{})                 {}
+func (n *nullLogger) Printf(format string, v ...interface{}) {}
+func (n *nullLogger) Println(v ...interface{})               {}
